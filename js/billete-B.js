c071547fd9aad3567df372ab195e751906f7980d
darredondort <darredondort@gmail.com>
2023-05-15 10:21:00 -0600

    agregar variable global clasificador

--- js/billete-B.js
+++ js/billete-B.js
@@ -6,6 +6,7 @@
 let localidad = "San Luis Potosí";
 let uso = "gastó";
 let año = "2021";
+let clasificador = "Clasificador por Objeto del Gasto"
 
 // Categorías COG
 const typeLabels = ["Servicios personales", "Materiales y suministros", "Servicios generales", "Inversiones financieras y otras provisiones", "Deuda pública", "Participaciones y aportaciones", "Bienes muebles, inmuebles e intangibles", "Transferencias, asignaciones, subsidios y otras ayudas", "Inversión pública"];
